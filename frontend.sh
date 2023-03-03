f581c196bad8f89fb3c45f2e2b58b5e87c6e5b29
shiva-charan-git <shivacharan021@gmail.com>
2023-03-03 13:51:48 +0530

    m

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,23 @@
+echo -e "\e[35mIntalling Ngix\e[0m"
 yum install nginx -y 
+
+echo -e "\e[35mRemoveing old content \e[0m"
 rm -rf /usr/share/nginx/html/* 
+
+echo -e "\e[35m Downloading fronend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+
+echo -e "\e[35m extracting downloaded frontend\e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
+
+echo -e "\e[35mCopying Ngix config for Roboshop[0m"
 cp config/nginx-roboshop.conf //etc/nigix/default.d/roboshop.conf
+
+echo -e "\e[35mEnabling Nginx\e[0m"
 systemctl enable nginx 
+
+echo -e "\e[35mRestarting Nginx\e[0m"
 systemctl restart nginx 
 
 ###Roboshop  config file is not placed yet
