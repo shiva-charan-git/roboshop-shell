3450e73462a4e378aecffefb509b533adeb350f8
shiva-charan-git <shivacharan021@gmail.com>
2023-03-03 16:45:44 +0530

    M

--- frontend.sh
+++ frontend.sh
@@ -16,8 +16,7 @@ unzip /tmp/frontend.zip
 
 echo -e "\e[35mCopying Ngix config for Roboshop\e[0m"
 
-pwd
-ls -l
+
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nigix/default.d/roboshop.conf
 
 echo -e "\e[35mEnabling Nginx\e[0m"
